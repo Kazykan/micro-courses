ae345ae7c9e1fa6b1e84122618cb008170203232
Kazykan <rufat@kocherbaev.ru>
2024-02-14 23:55:25 +0300

    add playwright test

---
+++ core/tests/courses-list.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from "@playwright/test";
+
+test("create delete course list", async ({ page }) => {
+  await page.goto("/");
+  await page.getByPlaceholder("называние").click();
+  await page.getByPlaceholder("называние").fill("Test course");
+  await page.getByPlaceholder("описание").click();
+  await page.getByPlaceholder("описание").fill("Test description");
+  await page.getByRole("button", { name: "Добавить" }).click();
+  await expect(
+    page.getByText("Test courseTest descriptionУдалить"),
+  ).toBeVisible();
+
+  await page.getByRole("button", { name: "Удалить" }).click();
+
+  await expect(
+    page.getByText("Test courseTest descriptionУдалить"),
+  ).not.toBeVisible();
+});
