10d9f85917614bf10ec2978b1679132206b963a9
Kazykan <rufat@kocherbaev.ru>
2024-02-18 23:38:05 +0300

    main: fix padding

--- core/src/app/page.tsx
+++ core/src/app/page.tsx
@@ -3,7 +3,7 @@ import { CreateCourseForm } from "@/features/courses-list/pub/create-course-form
 
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col p-4">
+    <main className="flex min-h-screen flex-col p-5">
       <h1>Courses</h1>
       <CreateCourseForm revalidatePagePath="/" className="max-w-[300px] mb-10" />
       <CoursesList revalidatePagePath="/" />
