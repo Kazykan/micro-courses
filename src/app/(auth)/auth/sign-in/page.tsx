516397e7a90e740a38474d0c8d9b46ca87ea05a1
Kazykan <rufat@kocherbaev.ru>
2024-02-23 16:47:20 +0300

    feat-header: add header

---
+++ src/app/(auth)/auth/sign-in/page.tsx
@@ -0,0 +1,3 @@
+export default function SignIn() {
+  return <div className="container py-18">Sign In</div>;
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Inter as FontSans } from "next/font/google";
 import { cn } from "@/shared/ui/utils";
+import { ThemeProvider } from "@/features/theme/theme-provider";
+import { AppProvider } from "./_providers/app-provider";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,14 +21,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
           fontSans.variable,
         )}
       >
-        {children}
+        <AppProvider>{children}</AppProvider>
       </body>
     </html>
   );
